e0a954f5154fc0bf0277031ce891c950c5c4fd96
c4rl0s01 <barahona5carlos@gmail.com>
2023-04-12 00:09:16 +0200

    ahora el ej2 fufa con java y python

--- Practica2/ej2.java
+++ Practica2/ej2.java
@@ -6,7 +6,7 @@ public class ej2 {
 
         Scanner lector = new Scanner(System.in);
         String frase = lector.nextLine();
-        Pattern pattern = Pattern.compile("(\\bE[-|\\s\\S]?[0-9]{4}[|\\s|\\-]?[A-Z]{3})|([0-9]{4}[-|\\s\\S]?[A-Z]{3}\\b)");
+        Pattern pattern = Pattern.compile("(\\bE[-|\\s|\\S]?[0-9]{4}[|\\s|\\-]?[A-Z]{3})|([0-9]{4}[-|\\s|\\S]?[A-Z]{3}\\b)");
         Matcher matcher= pattern.matcher(frase);
         int len = frase.length();
         while ( matcher.find()){
--- Practica2/ej2.py
+++ Practica2/ej2.py
@@ -3,10 +3,13 @@ import re
 #Mi tractor tiene la matricula E-1234 ABC, mi coche 0011AAB.
 
 new = input()
-pattern = r'(\bE[-|\s\S]?[0-9]{4}[|\s|\-]?[A-Z]{3})|([0-9]{4}[-|\s\S][A-Z]{3}\b)'
+pattern = r'(\bE[-|\s|\S]?[0-9]{4}[|\s|\-]?[A-Z]{3})|([0-9]{4}[-|\s|\S]?[A-Z]{3}\b)'
 
 x = re.findall(pattern, new)
 
 
 for match in x:
-    print(*match)
+    if match[1] != '':
+        print(match[1])
+    elif match[0] != '':
+        print(match[0])
