6acbaf53a239335483309d0c547db6250ab92eb5
angel <a.delcastillo.2020@alumnos.urjc.es>
2023-04-12 17:21:14 +0200

    Exercise 5 solved

--- Practica2/ej5.py
+++ Practica2/ej5.py
@@ -2,7 +2,7 @@ import re
 
 new = input()
 
-pattern = r"(Calle|C\/)\s([A-Z][a-z]+),?\s+(Nº|nº7|N\b|n\b|\b|Nº\b|nº\b|n|N)(\d+),\s+([0-9]{5})"
+pattern = r"\b(C\/|Calle)\s([A-ZÁÉÍÓÚÑ][a-záéíóúñ]+),?\s+(Nº|nº|N\s|n\s|Nº\s|nº\s|n|N)?(\d+),\s+([0-9]{5})\b"
 results = re.findall(pattern, new)
 for match in results:
     print(match[4], match[1], match[3], sep='-')
