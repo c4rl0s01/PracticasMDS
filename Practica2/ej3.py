98fb97ead899717b8f1a2eadb2c3d3e0db51137e
angel <a.delcastillo.2020@alumnos.urjc.es>
2023-04-12 16:03:10 +0200

    Exercise 3 solved

--- Practica2/ej3.py
+++ Practica2/ej3.py
@@ -2,7 +2,7 @@ import re
 
 new = input()
 
-pattern = r"(.*\b)(\d{4}\-[0,1][0-9]\-[0-3][0-9])(\b.*)"
-results = re.search(pattern, new)
-results_replaced = re.sub('-', '.', results.group(2)).split('.')
-print(results.group(1)+results_replaced[2]+'.'+results_replaced[1]+'.'+results_replaced[0]+results.group(3))
+pattern = r"\b(\d{4})-(\d{2})-(\d{2})\b"
+sub_pattern = r'\3.\2.\1'
+results_replaced = re.sub(pattern, sub_pattern, new)
+print(results_replaced)
